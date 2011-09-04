ee1b0131db6a79473eb7bf492c6a170e4f69e4eb
Jose Sibande <jbsibande@gmail.com>
2011-09-04 17:38:08 +0200

    Restructured project.

---
+++ blog/common/utils.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+def admin_perm_required(f):
+    import web
+
+    def deco(self, *args, **kw):
+        if not web.google_accounts.is_current_user_admin():
+            return web.seeother('/', absolute=True)
+        return f(self, *args, **kw)
+    return deco
+
+
+def markdown(content, safe="unsafe"):
+    """Format and highlight text"""
+    from markdown import markdown
+
+    if safe == "unsafe":
+        safe = False
+    else:
+        safe = 'escape'
+    content = markdown(content, ['codehilite',], safe,)
+    return content
+    
+
+
+template_tags = {'markdown': markdown}
+
+def render_template(template_name, **context):
+
+    import os
+    import datetime
+    from jinja2 import Environment,FileSystemLoader
+
+    extensions = context.pop('extensions', [])
+    globals = context.pop('globals', {})
+
+    jinja_env = Environment(
+            loader=FileSystemLoader(os.path.join(os.path.dirname(
+                    os.path.dirname(__file__)), 'templates')),
+            extensions=['jinja2.ext.autoescape'],
+            )
+    for v, k in template_tags.iteritems():
+        jinja_env.filters[v] = k
+
+    jinja_env.globals.update(globals)
+
+    #jinja_env.update_template_context(context)
+    return jinja_env.get_template(template_name).render(context)
